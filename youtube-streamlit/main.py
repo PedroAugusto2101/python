8e06009754d58a8ee8b660f078f81c3b9e10b947
PedroAugusto2101 <pedrotiagobh@gmail.com>
2024-05-21 17:27:19 -0300

    20240521-01-criacao-arquivo-youtube-streamlit

---
+++ youtube-streamlit/main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Hi! I am a Streamlit Web App")
+st.subheader("Hi! I am your Subheader")
+st.header("I am Header")
+st.text("Hi, I am a text functions and programmers use me inplace os paragraph tag.")
