8ea1fa5011ea7cb6fc944ad5f5e5d1d6a3c2ee1b
PedroAugusto2101 <pedrotiagobh@gmail.com>
2024-05-28 20:53:42 -0300

    Alteração para manter a constância de commits

--- hashtag-streamlit/app.py
+++ hashtag-streamlit/app.py
@@ -6,6 +6,7 @@ st.set_page_config(page_title="Meu Site Streamlit")
 
 with st.container():
     st.subheader("Meu primeiro site com o Streamlit!")
+    st.subheader("Meu primeiro site com o Streamlit!")
     st.title("Dashboard de Contratos")
     st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de maio")
     st.write("Quer aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/?origemurl=75502579145&gad_source=1&gclid=CjwKCAjwupGyBhBBEiwA0UcqaJ-UUZ-UZX1TWbWctgyaFOuRaf1DR0jsVlwPIwfEJ2wyXTLSUOaVchoCwTwQAvD_BwE)")
