7a3072de4c990032f6e55ec857e3a6a4fc22485d
PedroAugusto2101 <pedrotiagobh@gmail.com>
2024-06-06 21:13:21 -0300

    Criação arquivo funções.py

---
+++ OOP/home/jogos/funcoes.py
@@ -0,0 +1,18 @@
+def velocidade_media(distancia, tempo):
+    velocidade = distancia/tempo
+    return velocidade
+
+def soma(num1, num2):
+    resultado_soma = num1 + num2
+    return resultado_soma
+
+def subtracao(num1, num2):
+    resultado_subtracao = num1 + num2
+    return resultado_subtracao
+
+def calculadora(num1, num2):
+    resultado_soma = num1 + num2
+    resultado_subtracao = num1 - num2
+    resultado_multiplicacao = num1 * num2
+    resultado_divisao = num1 / num2
+    return resultado_soma, resultado_subtracao
