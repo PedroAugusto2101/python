416c18012794784dfe521fecbad77f5f62013db7
PedroAugusto2101 <pedrotiagobh@gmail.com>
2025-08-01 19:54:02 -0300

    aritmetic_operators

---
+++ 01_syntax/arithmetic_operators.py
@@ -0,0 +1,50 @@
+# Addition
+addition = 10 + 10
+print('Addition', addition)
+
+# Subtraction
+subtraction = 10 - 5
+print('Subtraction', subtraction)
+
+# Multiplication
+multiplication = 10 * 10
+print('Multiplication', multiplication)
+
+# Division (float)
+division = 10 / 3
+print('Division', division)
+
+# Integer division
+integer_division = 10 // 3
+print('Integer Division', integer_division)
+
+# Exponentiation
+exponentiation = 2 ** 10
+print('Exponentiation', exponentiation)
+
+# Modulo (remainder)
+modulo = 55 % 2
+print('Modulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
+
+# Concatenation
+concatenation = 'Pedro' + ' ' + 'Augusto'
+print(concatenation)
+
+a_ten_times = 'A' * 10
+three_times_pedro = 3 * 'Pedro'
+print(a_ten_times)
+print(three_times_pedro)
+
+# Operator precedence
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# 4. + -
+expression_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
+print(expression_1)
