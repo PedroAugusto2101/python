38ea9f57ef3afd6533f8ec61dafbb8087296e474
PedroAugusto2101 <pedrotiagobh@gmail.com>
2025-08-01 19:43:20 -0300

    add comments and variable section to python

---
+++ 01_syntax/comments.py
@@ -0,0 +1,14 @@
+"""
+DocString
+I can write
+what I want to
+
+testtest
+"""
+
+''' use to write your notes '''
+
+# Allows you to write a comment
+print(123) # In front of
+# Under
+print(456)
---
+++ 01_syntax/variables.py
@@ -0,0 +1,16 @@
+# Variables are used to store something in the computer's memory.
+# PEP8: start variable names with lowercase letters; you can use numbers and underscores _.
+# The = sign is the assignment operator. It is used toassign a value to a name (variable).
+# Usage: variable_name = expression
+
+# full_name = 'Pedro Augusto Andrade Silva'
+# sum_two_plus_two = 2 + 2
+# int_one = bool('1')
+# print(int_one, type(int_one))
+# print(full_name, sum_two_plus_two)
+
+name = 'Pedro'
+age = 22
+is_adult = age >= 18
+print('Name:', name, '\nAge:', age)
+print('Is adult?', is_adult)
